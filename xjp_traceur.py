27fa6a5874ff89c7a3a1ae157412132e1baa701d
The Policybot Lab <74462173+policybot2020@users.noreply.github.com>
2021-03-26 14:14:44 +0100

    Update xjp_traceur.py

--- xjp_traceur.py
+++ xjp_traceur.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Policybot.io 
+
+# This file is part of Policybot.
+#
+#    xjp traceur is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    xjp traceur is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with pyComtrade.  If not, see <http://www.gnu.org/licenses/>.
+# ====================================================================
+
+
 from sys import exit
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
